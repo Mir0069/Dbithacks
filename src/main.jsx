66cded2cb659d1d86d5636e52bdd1686b51b40e8
Yash Ravindra Mali <yashrmali3@gmail.com>
2025-03-02 03:28:52 +0530

    err

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 import './index.css'; // Assuming you have Tailwind CSS or any other styles here
 import { UserContext, UserProvider } from './context/Usercontext';
+import { SuccessProvider } from './context/Successcontext';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
